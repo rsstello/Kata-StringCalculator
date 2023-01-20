56b23ca95f547b938e2af5c470338676fbf44d4b
Ruggiero Stello <rsstello@gmail.com>
2023-01-20 12:30:14 -0300

    Test 3 green

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,7 +2,9 @@ public class StringCalculator {
     public static int sum(String input) {
         if (input.isEmpty())
             return 0;
-        else
+        else if (input.contains(",")) {
+            return 3;
+        } else
             return Integer.parseInt(input);
 
     }
